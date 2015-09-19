3f3c2b31762435b1d918e912aa948a8ea75c1034
Andrew Roper <andrewroper@gmail.com>
2015-09-19 17:03:28 -0400

    Working converter

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,55 @@
 var express = require('express');
 var router = express.Router();
 
+var ffmpeg = require('fluent-ffmpeg');
+var command = ffmpeg();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	testCmd();
   res.render('index', { title: 'Express' });
 });
+
+
+function testCmd() {
+	//ffmpeg -f lavfi -i color=c=blue:s=320x240:d=0.5 -vf "drawtext=fontfile=./feltthat.ttf:fontsize=30: \
+	//fontcolor=white:x=(w-text_w)/2:y=(h-text_h-line_h)/2:text='Hey, what's good?'" output.mp4
+	console.log("Running ffmpeg");
+	/*ffmpeg('color=c=blue:s=320x240:d=0.5')
+		.output('../public/output/quote.mp4');
+		*/
+	ffmpeg()
+		.input('/tmp/woodsmall.jpg').loop('1:00')
+		.videoFilters({
+  			filter: 'drawtext',
+  			options: {
+  				fontfile:'./feltthat.ttf',
+  				text: 'Hey what is GOOD!?',
+  				fontsize: 30,
+  				fontcolor: 'red',
+  				x: '(w-text_w)/2',
+  				y: '(h-text_h-line_h)/2',
+  				shadowcolor: 'black',
+  				shadowx: 2,
+  				shadowy: 2
+  			}
+  		})
+  		.videoCodec('libx264')
+  		.addOption('-pix_fmt', 'yuv420p')
+
+  		/*.input({
+
+  		})*/
+
+		//.(-vf "drawtext=fontfile=./feltthat.ttf:fontsize=30:fontcolor=white:x=(w-text_w)/2:y=(h-text_h-line_h)/2:text=\'Hey, what\'s good?\'")
+		.save('/tmp/output.mp4')
+		.on('end', function() {
+    		console.log('file has been converted succesfully');
+  		})
+  		.on('error', function(err) {
+    		console.log('an error happened: ' + err.message);
+  		});
+	//console.log("DONE Running ffmpeg");
+}
 
 module.exports = router;
